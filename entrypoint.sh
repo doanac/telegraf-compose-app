bef6797be558c996819d87a23b4d8b6c241e6d6f
Andy Doan <andy@foundries.io>
2025-01-24 11:07:55 -0600

    probe for sota config

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ UUID=$(openssl x509 -in /var/sota/client.pem -noout -subject | sed 's/^subject.*
 echo == Device UUID: $UUID
 echo == Generating telegraf config
 
+pkey=$(cat /var/sota/sota.toml | grep tls_pkey_path | cut -d\" -f2)
+client=$(cat /var/sota/sota.toml | grep tls_clientcert_path | cut -d\" -f2)
+ca=$(cat /var/sota/sota.toml | grep tls_cacert_path | cut -d\" -f2)
+
 cat >/tmp/telegraf.conf <<EOF
 [global_tags]
   uuid = "${UUID}"
@@ -31,9 +35,9 @@ percpu = false
   organization = "fio"
   bucket = "fio"
 
-  tls_ca = "/var/sota/root.crt"
-  tls_cert = "/var/sota/client.pem"
-  tls_key = "/var/sota/pkey.pem"
+  tls_ca = "${ca}"
+  tls_cert = "${client}"
+  tls_key = "${pkey}"
 EOF
 
 echo == Launching telegraf
